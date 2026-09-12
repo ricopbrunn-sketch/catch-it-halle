9f06fa063b6bd094084f39ae89dbf5e9accc9c50
ricopbrunn-sketch <ricopbrunn@gmail.com>
2026-09-13 01:19:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,140 @@
-const CACHE = "catchit-halle-v3";
+const CACHE =
+  "catchit-halle-v10";
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+const STATIC_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
 
-  event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll([
-        "./manifest.webmanifest",
-        "./icon.svg"
-      ])
-    )
-  );
-});
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE)
-          .map((key) => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
+self.addEventListener(
+  "install",
+  event => {
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+    self.skipWaiting();
 
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
+    event.waitUntil(
+      caches
+        .open(CACHE)
+        .then(
+          cache =>
+            cache.addAll(
+              STATIC_FILES
+            )
+        )
+    );
+  }
+);
 
-        caches.open(CACHE).then((cache) => {
-          cache.put(event.request, copy);
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+      caches
+        .keys()
+        .then(keys =>
+
+          Promise.all(
+            keys
+
+              .filter(
+                key =>
+                  key !== CACHE
+              )
+
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+          )
+        )
+
+        .then(
+          () =>
+            self.clients.claim()
+        )
+    );
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    const request =
+      event.request;
+
+
+    if (
+      request.method !== "GET"
+    ) {
+      return;
+    }
+
+
+    const url =
+      new URL(
+        request.url
+      );
+
+
+    /*
+      Nur eigene PWA-Dateien behandeln.
+      INSA/Cloudflare-Anfragen niemals cachen.
+    */
+
+    if (
+      url.origin !==
+      self.location.origin
+    ) {
+      return;
+    }
+
+
+    event.respondWith(
+
+      fetch(request)
+
+        .then(response => {
+
+          if (
+            response.ok
+          ) {
+
+            const copy =
+              response.clone();
+
+
+            caches
+              .open(CACHE)
+              .then(
+                cache =>
+                  cache.put(
+                    request,
+                    copy
+                  )
+              );
+          }
+
+
+          return response;
+        })
+
+
+        .catch(
+          () =>
+            caches.match(
+              request
+            )
+        )
+    );
+  }
+);          cache.put(event.request, copy);
         });
 
         return response;
