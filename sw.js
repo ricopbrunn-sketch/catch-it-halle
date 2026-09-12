d135a580bddc5098ccdb713a26b68845eedbff08
ricopbrunn-sketch <ricopbrunn@gmail.com>
2026-09-12 23:51:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="catchit-halle-v1"; self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"])))); self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
